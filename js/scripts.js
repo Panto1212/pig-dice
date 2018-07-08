ae3636c35d904ce39de2c43ef4593b5e1a53f325
Panto1212 <patienceankundat@gmail.com>
2018-07-08 08:01:07 +0300

    add a player constructor

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//backend logic
+
+//player constructor
+var Player = function(name, points) {
+  this.name = name,
+  this.points = points
+};
+
+var random = Math.ceil(Math.random()*6);
+console.log(random)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//frontend logic
