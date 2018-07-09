9f0ffdfccab5d39b5b15875f1bef3aa44fb6302d
Panto1212 <patienceankundat@gmail.com>
2018-07-09 04:43:38 +0300

    add condition for when 1 shows up

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,41 @@
 //backend logic
 
-//player constructor
-var Player = function(name) {
-  this.name = name,
-  this.points = []
-}
+//generating the random numbers of the dice
 var diceRoll = function() {
-  var random = Math.floor(Math.random()*6 + 1);
+  var random = Math.floor(Math.random() * 6 + 1);
 }
 
+//player constructor
+var Player = function(status) {
+  this.name;
+  this.turnscore = 0;
+  this.totalscore = 0;
+  this.status = status;
+  this.roll = 0;
+}
 
+//in the event that a 1 is rolled,
+Player.prototype.rolling1 = function() {
+  if (this.roll === 1) {
+    this.turnscore = 0;
+    this.turn = false;
+  } else {
+    this.turnscore += this.roll;
+  }
+}
 
+//in the event that the player decides to hold
+Player.prototype.hold = function() {
+  this.totalscore += this.turnscore;
+  this.turnscore = 0;
+}
 
-
-
-
-
+//when a player wins
+Player.prototype.win = function() {
+  if(this.totalscore >= 100) {
+    alert(this.name + "wins the game!")
+  }
+}
 
 
 
@@ -28,22 +48,22 @@ var diceRoll = function() {
 
 //frontend logic
 $(document).ready(function() {
-$("form#player1").submit(function() {
-  event.preventDefault();
-  var player1input= $("input#player1").val();
-  var player1 = new Player(player1input);
-  $("div.player1").empty();
-$("p#player1").append(player1.name + " is Ready!");
-});
+  $("form#player1").submit(function() {
+    event.preventDefault();
+    var player1input = $("input#player1").val();
+    var player1 = new Player(player1input);
+    $("div.player1").empty();
+    $("p#player1").append(player1.name + " is Ready!");
+  });
 
 
-$("form#player2").submit(function() {
-  event.preventDefault();
-  var player2input= $("input#player2").val();
-  var player2 = new Player(player2input);
-  $("div.player2").empty();
-$("p#player2").append(player2.name + " is Ready!");
-});
+  $("form#player2").submit(function() {
+    event.preventDefault();
+    var player2input = $("input#player2").val();
+    var player2 = new Player(player2input);
+    $("div.player2").empty();
+    $("p#player2").append(player2.name + " is Ready!");
+  });
 
 
 
