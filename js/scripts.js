a17c336eb752f9a3c01cf519184fe4a18fb5fc67
Panto1212 <patienceankundat@gmail.com>
2018-07-09 04:01:18 +0300

    add player2 object

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,9 @@ var Player = function(name) {
   this.name = name,
   this.points = []
 }
-
-var random = Math.floor(Math.random()*6 + 1);
+var diceRoll = function() {
+  var random = Math.floor(Math.random()*6 + 1);
+}
 
 
 
@@ -32,11 +33,17 @@ $("form#player1").submit(function() {
   var player1input= $("input#player1").val();
   var player1 = new Player(player1input);
   $("div.player1").empty();
-$("p#player1").append(player1.name);
-
-
+$("p#player1").append(player1.name + " is Ready!");
 });
 
+
+$("form#player2").submit(function() {
+  event.preventDefault();
+  var player2input= $("input#player2").val();
+  var player2 = new Player(player2input);
+  $("div.player2").empty();
+$("p#player2").append(player2.name + " is Ready!");
+});
 
 
 
