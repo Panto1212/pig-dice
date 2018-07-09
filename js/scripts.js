b3fd957f0ca669815bb19971f3dc1542f9e8d9b0
Panto1212 <patienceankundat@gmail.com>
2018-07-09 06:00:14 +0300

    when hold is clicked, display total score

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 
 //generating the random numbers of the dice
 var diceRoll = function() {
-  var random = Math.floor(Math.random() * 6 + 1);
+  return Math.floor(Math.random() * 6 + 1);
 }
 
 //player constructor
-var Player = function(status) {
+function Player(status) {
   this.name;
   this.turnscore = 0;
   this.totalscore = 0;
@@ -18,7 +18,6 @@ var Player = function(status) {
 Player.prototype.rolling1 = function() {
   if (this.roll === 1) {
     this.turnscore = 0;
-    this.turn = false;
   } else {
     this.turnscore += this.roll;
   }
@@ -30,19 +29,15 @@ Player.prototype.hold = function() {
   this.turnscore = 0;
 }
 
-//when a player wins
+//when a player reaches 100
 Player.prototype.win = function() {
-  if(this.totalscore >= 100) {
+  if (this.totalscore >= 100) {
     alert(this.name + "wins the game!")
-  } else {
-    this.totalscore += this.turnscore;
   }
 }
-
-
-
 //frontend logic
 $(document).ready(function() {
+
   $("form#player1").submit(function() {
     event.preventDefault();
     $("div.player1").empty();
@@ -59,33 +54,52 @@ $(document).ready(function() {
   });
 
 
-$("span#start").click(function() {
-  $("div.game-mode").show();
-  $("div.playernames").hide();
-  $("span#start").hide();
-  var player1 = new Player(true);
-  var player2 = new Player(false);
-});
+  $("span#start").click(function(event) {
+    $("div.game-mode").show();
+    $("div.playernames").hide();
+    $("span#start").hide();
 
-var player1input = $("input#player1").val();
-$("h3.player1").append(player1input);
-player1.name = player1input;
+    player1 = new Player(true);
+    player2 = new Player(false);
 
-var player2input = $("input#player2").val();
-$("h3.player2").append(player2input);
-player2.name = player2input;
+    var player1input = $("input#player1").val();
+    $("h3.player1").append(player1input);
 
-  $("span.roll1").click(function(event) {
+    var player2input = $("input#player2").val();
+    $("h3.player2").append(player2input);
+
+    player1.name = player1input;
+    player2.name = player2input;
+
+  });
+
+  $("span#roll1").click(function(event) {
     player1.roll = diceRoll();
     $("span#rollnumber-1").text(player1.roll);
     player1.rolling1();
-    $("span#turnpoints-1").text(palyer1.turnscore);
+    $("span#turnpoints-1").text(player1.turnscore);
   });
 
-  $("span.roll2").click(function(event) {
+  $("span#roll2").click(function(event) {
     player2.roll = diceRoll();
     $("span#rollnumber-2").text(player2.roll);
     player2.rolling1();
-    $("span#turnpoints-2").text(palyer2.turnscore);
+    $("span#turnpoints-2").text(player2.turnscore);
+  });
+
+  $("span#hold1").click(function(event) {
+    player1.hold();
+    $("span#totalpoints-1").text(player1.totalscore);
+    $("span#rollnumber-1").empty();
+    $("span#turnpoints-1").empty();
+    player1.win();
+  });
+
+  $("span#hold2").click(function(event) {
+    player2.hold();
+    $("span#totalpoints-2").text(player2.totalscore);
+    $("span#rollnumber-2").empty();
+    $("span#turnpoints-2").empty();
+    player2.win();
   });
 });
